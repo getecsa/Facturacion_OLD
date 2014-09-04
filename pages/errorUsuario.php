6ef80265650f564cc6316cd80717c816891bd068
getecsa <getecsamexico@gmail.com>
2014-09-04 10:34:10 -0500

    ldap correcto

--- pages/errorUsuario.php
+++ pages/errorUsuario.php
@@ -1,4 +1,4 @@
-   <div id="divNotificacion" />
+<div id="divNotificacion" />
 <div class="contenedor">
             <div class="header">
                 <img alt="Movistar" class="logotipo" src="images/logo.png" />
@@ -6,8 +6,16 @@
             </div>
             <div class="content">
                 <div class="login">
-                    <h2>Failed User...</h2>
-                </div>
+ <?php
+if($_GET["Error"]==1)
+    echo "<h2>Error 01: Usuario ó Contraseña Invalido</h2>";
+
+if($_GET["Error"]==2)
+    echo "<h2>Error 02: Usuario no existe en servidor Mexico Team MX</h2>";
+
+ ?>
+
+               </div>
             </div>
 </div>
 </div>
--- pages/inicio.php
+++ pages/inicio.php
@@ -2,56 +2,86 @@
 
 if (isset($_POST['txtUsuario'])){
    
-    include("conectar_bd.php");  
-    conectar_bd();
+    //ldap
+  
+  /*  $usuario = $_POST['txtUsuario'];
+    $pass = $_POST['txtContraseña'];
+    
+                              // $usuario = 'MRT06294';
+                              // $pas = 'Rodrigo.09';
+                               $ad_host = 'mexico.tem.mx';
+                                    $ds = ldap_connect('mexico.tem.mx');
+                if (!$ds) throw new Exception('No se pudo conectar al servidor mexico.tem.mx');
+                define('LDAP_OPT_DIAGNOSTIC_MESSAGE', 0x0032);
+                ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+                ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
+                                $valor=explode('.',$ad_host);
+                                $user = $valor[0].'\\'.$usuario;
+                                $bind = ldap_bind($ds,$user,$pass);                                                                                                               
+ 
+if($bind)
+*/
+
+   $usuario = $_POST['txtUsuario'];
+   $pass = $_POST['txtContraseña'];
+   //$usuario = 'MRT06294';
+   //$pas = 'Rodrigo.09';
+   $ldaphost = 'mexico.tem.mx';
+   $ds=ldap_connect($ldaphost);  
+   $valor=explode('.',$ldaphost);
+   $user = $valor[0].'\\'.$usuario;
+ 
+if ($ds) {
+          //realizando la autenticacion
+          $r=ldap_bind($ds, $user, $pass);
+          if(!$r) die("Error: Usuario y Contraseña no existen en servidor principal<br>");
+
+    include("config.php");
      
-    $usr = $_POST['txtUsuario'];
-    $pw = $_POST['txtContraseña'];
-
-
     $sql ="SELECT   id_usuario, username, nombre, n_paterno, n_materno,
                     users.area_idarea as id_area, area.tx_area as tx_area, area.oper_sol as oper_sol
              FROM   users
        INNER JOIN   area
                ON   users.area_idarea = area.id_area
-            WHERE   username =  '$usr'
-              AND   pass = '$pw' ";
+            WHERE   username =  '$usuario'";
 
-    $result = mysql_query($sql,$conexion); 
+     $result=mysql_db_query($db, $sql, $link); 
 
     $uid = "";
      
     //Si existe al menos una fila
     if( $fila=mysql_fetch_array($result) )
-    {       
+       {       
         
-        $uid = $fila['id_usuario'];
-        $id_area = $fila['id_area'];
-        session_start();  
-        $_SESSION['autenticado']    = 'SI';
-        $_SESSION['uid']            = $uid;
-        $_SESSION['username']       = $fila['username'];
-        $_SESSION['area']        = $fila['id_area'];
-        $_SESSION['tx_area']        = $fila['tx_area'];
-        $_SESSION['nombre']        = $fila['nombre'];
-        $_SESSION['paterno']        = $fila['n_paterno'];
-        $_SESSION['materno']        = $fila['n_materno'];
-        $_SESSION['oper_sol']        = $fila['oper_sol'];
+                $uid = $fila['id_usuario'];
+                $id_area = $fila['id_area'];
+                session_start();  
+                $_SESSION['autenticado']    = 'SI';
+                $_SESSION['uid']            = $uid;
+                $_SESSION['username']       = $fila['username'];
+                $_SESSION['area']        = $fila['id_area'];
+                $_SESSION['tx_area']        = $fila['tx_area'];
+                $_SESSION['nombre']        = $fila['nombre'];
+                $_SESSION['paterno']        = $fila['n_paterno'];
+                $_SESSION['materno']        = $fila['n_materno'];
+                $_SESSION['oper_sol']        = $fila['oper_sol'];
 
-     if($fila['oper_sol']==0){
-                header('Location: index.php?id=operador');
-        }
-        else {
-             header('Location: index.php?id=solicitante');
-        }
-                 
-               
+             if($fila['oper_sol']==0){
+                        header('Location: index.php?id=operador');
+                }
+                else {
+                     header('Location: index.php?id=solicitante');
+                }
         
-    }
+        }
 
-    else {
-        header('Location: index.php?id=errorUsuario');
-    }
+            else  {
+                header('Location: index.php?id=errorUsuario&Error=1');
+                    }
+
+ } else {
+                echo "Error en conexion del servidor"; 
+                    }
 
 }
     else{
@@ -86,7 +116,7 @@ if (isset($_POST['txtUsuario'])){
                       <p>
                         
                         Contraseña:
-                        <input type = "password" id="txtContraseña" name="txtContraseña" MaxLength="20"/>
+                        <input type = "password" id="txtContraseña" name="txtContraseña" MaxLength="20" required/>
                     
                     </p>
                     <p>
